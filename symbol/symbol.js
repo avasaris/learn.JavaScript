d6d17091f734be22cdbd47cb8b8e8e0ea634a29d
avasaris <artvas@yandex.ru>
2020-01-05 16:28:17 +0200

    symbols examples

--- symbol/symbol.js
+++ symbol/symbol.js
@@ -3,29 +3,44 @@ let id2 = Symbol("id");
 
 console.log(id1 === id2);
 
+console.log(id1.toString());
+console.log(id1);
 
 let id3 = Symbol.for("iidd");
 let id4 = Symbol.for("iidd");
 
-console.log(id2 === id3);
 console.log(id3 === id4);
 
-console.log(id1.toString());
-console.log(id1);
+console.log("KeyFor id4 :", Symbol.keyFor(id4));
 
 let obj = {
     id3: "Hide me 1",
     [id3]: "Hide Me 2"
 }
 
-console.log(obj[id3]);
+console.log("Obj: ", obj[id3]);
 
-for(key in obj){
-    console.log("KEY:", key);
+console.log("for...in");
+for (key in obj) {
+    console.log("KEY :", key);
 }
 
+console.log('console.table(obj)');
 console.table(obj);
 
+console.log("Reflect.ownKeys(obj)");
 console.table(Reflect.ownKeys(obj));
 
-console.log("KeyFor id4 :", Symbol.keyFor(id4));
+console.log("Object.getOwnPropertySymbols(obj)");
+console.table(Object.getOwnPropertySymbols(obj));
+
+let cloneObj = Object.assign({}, obj);
+
+console.log('console.table(cloneObj)');
+console.table(cloneObj);
+
+console.log("Reflect.ownKeys(cloneObj)");
+console.table(Reflect.ownKeys(cloneObj));
+
+console.log("Object.getOwnPropertySymbols(cloneObj)");
+console.table(Object.getOwnPropertySymbols(cloneObj));
